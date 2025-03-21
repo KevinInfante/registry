0d0dbf5e1144b9c8041c6469fdf72d220aadae16
Kevin Infante <kevininfante90@gmail.com>
2025-03-21 18:57:45 -0400

    Changed the update function to use postgres, it seemingly works now.

--- backend/server.js
+++ backend/server.js
@@ -66,10 +66,10 @@ app.get('/searching/:search', async (req, res) => {
     let search = req.params.search; //should be a number
     console.log("search: ",search);
     try{
-        var result = await client.query("SELECT * WHERE number = $1", [search])
+        var result = await client.query("SELECT * FROM clients WHERE number = $1", [search])
         if(result.rows.length>0) {
             console.log("found");
-            res.status(200).send(result);
+            res.status(200).send(result.rows[0]);
         }
         else{
             console.log("result: ", result);
@@ -107,27 +107,20 @@ app.delete('/delete', async(req, res)=>{
 // .put or .post is probably identical, that's how it is in axios
 app.put('/update/:number', async (req, res) =>{
     console.log("body: ",req.body); // body: {number: '111...', dates: [...]}
-    let query = { "number": req.body.number };
-    var customer = { //only sending the dates bc that's what I want updated
-        "date": req.body.dates
-    }
-    try{
-        await client.connect();
-        var result = await dbo.collection(db.collection).updateOne(query, {$set: customer});
-        if(result){
-            console.log("result: ", result);
-            res.status(200).send(result);
+    try{   //client.query("UPDATE clients SET date = $1 WHERE number = $2")
+        var result = await client.query("UPDATE clients SET date = $1 WHERE number = $2 RETURNING *;",
+            [req.body.dates, req.body.number]);
+        if(result.rows.length>0){
+            console.log("result: ", result.rows);
+            res.status(200).send(result.rows);
         }  else{
-            console.log("NULL RESULT? ", result);
+            console.log("NULL RESULT? ");
             throw new Error("update failed");
         }
     } catch(err){
         console.log("error: ", err);
         res.status(400).send(err);
-
-    } finally {
-        client.close();
-    }
+    } 
 })
 
 app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
